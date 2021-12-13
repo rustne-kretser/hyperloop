rustne-kretser/hyperloop

commit b97f3cea976eb3f47acbfb5fd72d289fd1ba3b50

Implemented statically allocated lock free MPSC priority queue
- Priority queue based on binary heap

- When items are added, they are pushed to a stack at the tail end of
  the buffer (with the heap located at the other end). When popping an
  item from the queue, first all items on the stack are inserted into
  the heap and then the highest priority item is extracted from the
  heap. This allow for atomic pushing to the queue from multiple
  producers with complex processing done in a single consumer.

- There is a major caveat at the moment. Producers can safely pre-empt
  eachother and pre-empt the consumer, but the consumer can't pre-empt
  producers.

hyperloop/src/executor.rs
@@ -1,8 +1,8 @@
 use core::cmp::Ordering;
 
-use heapless::binary_heap::Max;
+use crate::priority_queue::{Max, PriorityQueue, Sender};
 
-use crate::{priority_channel::{Item, Receiver, Sender, channel}, task::PollTask};
+use crate::task::PollTask;
 
 pub(crate) type Priority = u8;
 
@@ -11,8 +11,6 @@ pub struct Ticket {
     task: *const dyn PollTask,
     priority: Priority,
 }
-
-impl Item for Ticket {}
 
 impl Ticket {
     pub(crate) fn new(task: *const dyn PollTask, priority: Priority) -> Self {
@@ -47,21 +45,14 @@ impl Ord for Ticket {
     }
 }
 
-pub(crate) type TaskSender = Sender<Ticket>;
-pub(crate) type TaskReceiver<const N: usize> = Receiver<Ticket, Max, N>;
-
 pub struct Executor<const N: usize> {
-    sender: TaskSender,
-    receiver: TaskReceiver<N>,
+    queue: PriorityQueue<Ticket, Max, N>,
 }
 
 impl<const N: usize> Executor<N> {
     pub fn new() -> Self {
-        let (sender, receiver) = channel();
-
         Self {
-            sender,
-            receiver,
+            queue: PriorityQueue::new(),
         }
     }
 
@@ -75,13 +66,13 @@ impl<const N: usize> Executor<N> {
     /// be dereferenced at any time and will be dangling if the
     /// exeutor is moved or dropped.
     pub unsafe fn poll_tasks(&mut self) {
-        while let Ok(ticket) = self.receiver.recv() {
+        while let Some(ticket) = self.queue.pop() {
             let _ = ticket.get_task().poll();
         }
     }
 
-    pub fn get_sender(&self) -> TaskSender {
-        self.sender.clone()
+    pub fn get_sender(&self) -> impl Sender<Item = Ticket> {
+        self.queue.get_sender()
     }
 }
 

commit 7fd3362d8fa7a07ad5092804f8781b72948a0113

Removed generics (except for T) from PrioritySender
- Commit b97f3ce broke the `task` attribute macro by adding generic
  type for `Sender` from priority queue, requiring that the concrete
  type of the executor is known to create a task.

- Re-factored `PrioritySender`, removing overly specific generics such
  as queue size and heap kind that are not strictly needed for the
  sender. Re-introduced `TaskSender` and reverted code using
  `Sender`-trait.

hyperloop/src/executor.rs
@@ -1,6 +1,6 @@
 use core::cmp::Ordering;
 
-use crate::priority_queue::{Max, PriorityQueue, Sender};
+use crate::priority_queue::{Max, PriorityQueue, PrioritySender};
 
 use crate::task::PollTask;
 
@@ -45,6 +45,8 @@ impl Ord for Ticket {
     }
 }
 
+pub(crate) type TaskSender = PrioritySender<Ticket>;
+
 pub struct Executor<const N: usize> {
     queue: PriorityQueue<Ticket, Max, N>,
 }
@@ -71,7 +73,7 @@ impl<const N: usize> Executor<N> {
         }
     }
 
-    pub fn get_sender(&self) -> impl Sender<Item = Ticket> {
+    pub fn get_sender(&self) -> TaskSender {
         self.queue.get_sender()
     }
 }

hyperloop/src/priority_queue.rs
@@ -120,27 +120,77 @@ pub trait Sender: Clone {
     fn send(&self, item: Self::Item) -> Result<(), Self::Item>;
 }
 
-pub struct PrioritySender<'a, T, K, const N: usize>
-where T: PartialOrd,
-      K: Kind {
-    queue: &'a PriorityQueue<T, K, N>,
+pub struct PrioritySender<T>
+where T: 'static {
+    slots: &'static [Option<T>],
+    available: &'static AtomicUsize,
+    stack_size: &'static AtomicUsize,
 }
 
-impl<'a, T, K, const N: usize> Clone for PrioritySender<'a, T, K, N>
-where T: PartialOrd,
-      K: Kind {
+impl<T> Clone for PrioritySender<T> {
     fn clone(&self) -> Self {
-        Self { queue: self.queue.clone() }
+        Self {
+            slots: self.slots.clone(),
+            available: self.available.clone(),
+            stack_size: self.stack_size.clone() }
     }
 }
 
-impl<'a, T, K, const N: usize> Sender for PrioritySender<'a, T, K, N>
-where T: PartialOrd + 'static,
-      K: Kind + 'static {
+impl<T> PrioritySender<T> {
+    unsafe fn slot_mut(&self, index: usize) -> &mut Option<T> {
+        &mut *((&self.slots[index]
+                as *const Option<T>)
+               as *mut Option<T>)
+    }
+
+    fn stack_push(&self, item: T) -> Result<(), T> {
+        loop {
+            let stack_size = self.stack_size.load(Ordering::Relaxed);
+
+            if stack_size < self.slots.len() {
+                if let Ok(_) = self.stack_size.compare_exchange(stack_size, stack_size + 1,
+                                                                Ordering::Relaxed,
+                                                                Ordering::Relaxed) {
+                    let index = self.slots.len() - stack_size - 1;
+
+                    unsafe {
+                        let slot = self.slot_mut(index);
+                        *slot = Some(item);
+                    }
+
+                    break Ok(());
+                }
+            } else {
+                break Err(item);
+            }
+        }
+    }
+
+    pub fn push(&self, item: T) -> Result<(), T> {
+        loop {
+            let available = self.available.load(Ordering::Relaxed);
+
+            if available > 0 {
+                if let Ok(_) = self.available.compare_exchange(available, available - 1,
+                                                               Ordering::Relaxed,
+                                                               Ordering::Relaxed) {
+                    break;
+                }
+            } else {
+                return Err(item);
+            }
+        }
+
+        self.stack_push(item)
+    }
+
+}
+
+impl<T> Sender for PrioritySender<T> {
     type Item = T;
 
     fn send(&self, item: T) -> Result<(), T> {
-        self.queue.push(item)
+        self.push(item)
     }
 }
 
@@ -192,11 +242,13 @@ where T: PartialOrd + 'static,
         }
     }
 
-    pub fn get_sender(&self) -> impl Sender<Item = T> {
-        let queue = unsafe { &*(self as *const Self) };
+    pub fn get_sender(&self) -> PrioritySender<T> {
+        let queue: &'static Self = unsafe { &*(self as *const Self) };
 
         PrioritySender {
-            queue
+            slots: &queue.slots,
+            available: &queue.available,
+            stack_size: &queue.stack_size,
         }
     }
 
@@ -204,47 +256,6 @@ where T: PartialOrd + 'static,
         &mut *((&self.slots[index]
                 as *const Option<T>)
                as *mut Option<T>)
-    }
-
-    fn stack_push(&self, item: T) -> Result<(), T> {
-        loop {
-            let stack_size = self.stack_size.load(Ordering::Relaxed);
-
-            if stack_size < N - self.heap_size {
-                if let Ok(_) = self.stack_size.compare_exchange(stack_size, stack_size + 1,
-                                                                Ordering::Relaxed,
-                                                                Ordering::Relaxed) {
-                    let index = N - stack_size - 1;
-
-                    unsafe {
-                        let slot = self.slot_mut(index);
-                        *slot = Some(item);
-                    }
-
-                    break Ok(());
-                }
-            } else {
-                break Err(item);
-            }
-        }
-    }
-
-    pub fn push(&self, item: T) -> Result<(), T> {
-        loop {
-            let available = self.available.load(Ordering::Relaxed);
-
-            if available > 0 {
-                if let Ok(_) = self.available.compare_exchange(available, available - 1,
-                                                               Ordering::Relaxed,
-                                                               Ordering::Relaxed) {
-                    break;
-                }
-            } else {
-                return Err(item);
-            }
-        }
-
-        self.stack_push(item)
     }
 
     fn get_node(&self, index: usize) -> Node<T, K, N> {
@@ -433,12 +444,13 @@ mod tests {
     #[test]
     fn stack() {
         let mut heap: PriorityQueue<u32, Min, 10> = PriorityQueue::new();
+        let sender = heap.get_sender();
 
         for i in 0..10 {
-            heap.stack_push(i).unwrap();
+            sender.stack_push(i).unwrap();
         }
 
-        assert!(heap.stack_push(11).is_err());
+        assert!(sender.stack_push(11).is_err());
 
         for i in (0..10).rev() {
             assert_eq!(heap.pop_stack(), Some(i));
@@ -447,7 +459,7 @@ mod tests {
         assert!(heap.pop_stack().is_none());
 
         for i in 0..5 {
-            heap.stack_push(i).unwrap();
+            sender.stack_push(i).unwrap();
         }
 
         for i in (0..5).rev() {

hyperloop/src/task.rs
@@ -3,20 +3,16 @@ use core::{lazy::OnceCell, pin::Pin, task::{Context, Poll, RawWaker, RawWakerVTa
 use futures::Future;
 use log::error;
 
-use crate::{executor::{Priority, Ticket}, priority_queue::Sender};
+use crate::{executor::{Priority, TaskSender, Ticket}, priority_queue::Sender};
 
-unsafe fn clone<F, S>(ptr: *const ()) -> RawWaker
-where F: Future<Output = ()> + 'static,
-      S: Sender<Item = Ticket> + 'static {
-    let task = &*(ptr as *const Task<F, S>);
+unsafe fn clone<F: Future<Output = ()> + 'static>(ptr: *const ()) -> RawWaker {
+    let task = &*(ptr as *const Task<F>);
 
     RawWaker::new(ptr, &task.vtable)
 }
 
-unsafe fn wake<F, S>(ptr: *const ())
-where F: Future<Output = ()> + 'static,
-    S: Sender<Item = Ticket> + 'static {
-        let task = &*(ptr as *const Task<F, S>);
+unsafe fn wake<F: Future<Output = ()> + 'static>(ptr: *const ()) {
+    let task = &*(ptr as *const Task<F>);
     task.wake();
 }
 
@@ -27,18 +23,16 @@ pub(crate) trait PollTask {
     unsafe fn poll(&self) -> Poll<()>;
 }
 
-pub struct Task<F, S>
-where F: Future<Output = ()> + 'static,
-      S: Sender<Item = Ticket> {
+pub struct Task<F>
+where F: Future<Output = ()> + 'static {
     future: F,
     priority: Priority,
-    sender: OnceCell<S>,
+    sender: OnceCell<TaskSender>,
     vtable: RawWakerVTable,
 }
 
-impl<F, S> Task<F, S>
-where F: Future<Output = ()> + 'static,
-      S: Sender<Item = Ticket> + 'static {
+impl<F> Task<F>
+where F: Future<Output = ()> + 'static {
     pub fn new(
         future_fn: impl FnOnce() -> F,
         priority: Priority,
@@ -47,9 +41,9 @@ where F: Future<Output = ()> + 'static,
             future: future_fn(),
             priority,
             sender: OnceCell::new(),
-            vtable: RawWakerVTable::new(clone::<F, S>,
-                                        wake::<F, S>,
-                                        wake::<F, S>,
+            vtable: RawWakerVTable::new(clone::<F>,
+                                        wake::<F>,
+                                        wake::<F>,
                                         drop),
         }
     }
@@ -63,7 +57,7 @@ where F: Future<Output = ()> + 'static,
     }
 
     unsafe fn get_waker(&self) -> Waker {
-        let ptr: *const () = (self as *const Task<F, S>).cast();
+        let ptr: *const () = (self as *const Task<F>).cast();
         let vtable = &self.as_static().vtable;
 
         Waker::from_raw(RawWaker::new(ptr, vtable))
@@ -73,12 +67,12 @@ where F: Future<Output = ()> + 'static,
         self.schedule().unwrap();
     }
 
-    pub fn add_to_executor(&self, sender: S) -> Result<(), ()> {
+    pub fn add_to_executor(&self, sender: TaskSender) -> Result<(), ()> {
         self.set_sender(sender)?;
         self.schedule()
     }
 
-    fn set_sender(&self, sender: S) -> Result<(), ()> {
+    fn set_sender(&self, sender: TaskSender) -> Result<(), ()> {
         match self.sender.set(sender) {
             Ok(_) => Ok(()),
             Err(_) => Err(()),
@@ -101,9 +95,8 @@ where F: Future<Output = ()> + 'static,
     }
 }
 
-impl<F, S> PollTask for Task<F, S>
-where F: Future<Output = ()> + 'static,
-      S: Sender<Item = Ticket> + 'static {
+impl<F> PollTask for Task<F>
+where F: Future<Output = ()> + 'static {
     unsafe fn poll(&self) -> Poll<()> {
         let waker = self.get_waker();
         let mut cx = Context::from_waker(&waker);

hyperloop/src/timer.rs
@@ -6,7 +6,7 @@ use core::future::Future;
 use futures::{Stream, StreamExt, task::AtomicWaker};
 use log::error;
 
-use crate::priority_queue::{Min, PeekMut, PriorityQueue, Sender};
+use crate::priority_queue::{Min, PeekMut, PriorityQueue, PrioritySender, Sender};
 
 type Tick = u64;
 
@@ -101,17 +101,15 @@ impl Ord for Ticket {
     }
 }
 
-struct DelayFuture<S>
-where S: Sender<Item = Ticket> {
-    sender: S,
+struct DelayFuture {
+    sender: PrioritySender<Ticket>,
     counter: TickCounterToken,
     expires: Tick,
     started: bool,
 }
 
-impl<S> DelayFuture<S>
-where S: Sender<Item = Ticket> {
-    fn new(sender: S, counter: TickCounterToken, expires: Tick) -> Self {
+impl DelayFuture {
+    fn new(sender: PrioritySender<Ticket>, counter: TickCounterToken, expires: Tick) -> Self {
         Self {
             sender,
             counter,
@@ -121,11 +119,7 @@ where S: Sender<Item = Ticket> {
     }
 }
 
-impl<S> Unpin for DelayFuture<S>
-where S: Sender<Item = Ticket> {}
-
-impl<S> Future for DelayFuture<S>
-where S: Sender<Item = Ticket> {
+impl Future for DelayFuture {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
@@ -149,17 +143,16 @@ where S: Sender<Item = Ticket> {
     }
 }
 
-pub struct TimeoutFuture<S, F>
-where S: Sender<Item = Ticket>,
-      F: Future {
+pub struct TimeoutFuture<F>
+where F: Future {
     future: F,
-    delay: DelayFuture<S>,
+    delay: DelayFuture,
 }
 
-impl<S, F> TimeoutFuture<S, F>
-where S: Sender<Item = Ticket>,
-      F: Future {
-    fn new(future: F, sender: S, counter: TickCounterToken, expires: Tick) -> Self {
+impl<F> TimeoutFuture<F>
+where F: Future {
+    fn new(future: F, sender: PrioritySender<Ticket>,
+           counter: TickCounterToken, expires: Tick) -> Self {
         Self {
             future,
             delay: DelayFuture::new(sender, counter, expires),
@@ -167,9 +160,8 @@ where S: Sender<Item = Ticket>,
     }
 }
 
-impl<S, F> Future for TimeoutFuture<S, F>
-where S: Sender<Item = Ticket>,
-      F: Future {
+impl<F> Future for TimeoutFuture<F>
+where F: Future {
     type Output = Result<F::Output, ()>;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
@@ -192,16 +184,15 @@ where S: Sender<Item = Ticket>,
 }
 
 #[derive(Clone)]
-pub struct Timer<S>
-where S: Sender<Item = Ticket> {
+pub struct Timer
+where {
     rate: Hertz,
     counter: TickCounterToken,
-    sender: S,
+    sender: PrioritySender<Ticket>,
 }
 
-impl<S> Timer<S>
-where S: Sender<Item = Ticket> + Clone {
-    pub fn new(rate: Hertz, counter: TickCounterToken, sender: S) -> Self {
+impl Timer {
+    pub fn new(rate: Hertz, counter: TickCounterToken, sender: PrioritySender<Ticket>) -> Self {
         Self {
             rate,
             counter,
@@ -235,7 +226,7 @@ where S: Sender<Item = Ticket> + Clone {
     }
 
     pub fn timeout<F: Future>(&self, future: F, duration: Milliseconds)
-                   -> TimeoutFuture<S, F> {
+                   -> TimeoutFuture<F> {
         TimeoutFuture::new(future,
                            self.sender.clone(),
                            self.counter.clone(),
@@ -290,7 +281,7 @@ impl<const N: usize> Scheduler<N> {
         }
     }
 
-    pub fn get_timer(&self) -> Timer<impl Sender<Item = Ticket>> {
+    pub fn get_timer(&self) -> Timer {
         Timer::new(self.rate, self.counter.clone(), self.queue.get_sender())
     }
 
@@ -421,8 +412,8 @@ mod tests {
 
         let test_future = |queue, timer| {
             move || {
-                async fn future<S: Sender<Item = Ticket> + Clone>(queue: Arc<ArrayQueue<u32>>,
-                                                                  timer: Timer<S>) {
+                async fn future(queue: Arc<ArrayQueue<u32>>,
+                                timer: Timer) {
                     queue.push(1).unwrap();
 
                     timer.delay(0.milliseconds()).await;
@@ -521,12 +512,12 @@ mod tests {
 
         let waiting_future = |queue, timer| {
             move || {
-                async fn slow_future<S:Sender<Item = Ticket> + Clone>(timer: Timer<S>) {
+                async fn slow_future(timer: Timer) {
                     timer.delay(1000.milliseconds()).await;
                 }
 
-                async fn future<S: Sender<Item = Ticket> + Clone>(queue: Arc<ArrayQueue<u32>>,
-                                                                  timer: Timer<S>) {
+                async fn future(queue: Arc<ArrayQueue<u32>>,
+                                timer: Timer) {
                     queue.push(1).unwrap();
 
                     assert_eq!(timer.timeout(slow_future(timer.clone()), 100.milliseconds()).await,